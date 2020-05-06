b93f3a49eefba2b1769315fffe84afff78e0ca05
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-05-06 10:59:30 +0200

    Initial Commit

---
+++ SpiewnikWedrowca/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpiewnikWedrowca
+//
+//  Created by Timothy Stokarski on 06/05/2020.
+//  Copyright © 2020 Timothy Stokarski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
