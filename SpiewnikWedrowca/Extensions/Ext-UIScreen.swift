16df505186588346f6610bed04591d7d83fed0f9
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-05-07 16:47:09 +0200

    UIScreen extension

---
+++ SpiewnikWedrowca/Extensions/Ext-UIScreen.swift
@@ -0,0 +1,15 @@
+//
+//  Ext-UIScreen.swift
+//  SpiewnikWedrowca
+//
+//  Created by Timothy Stokarski on 07/05/2020.
+//  Copyright © 2020 Timothy Stokarski. All rights reserved.
+//
+
+import SwiftUI
+
+extension UIScreen {
+   static let width = UIScreen.main.bounds.size.width
+   static let height = UIScreen.main.bounds.size.height
+   static let size = UIScreen.main.bounds.size
+}
