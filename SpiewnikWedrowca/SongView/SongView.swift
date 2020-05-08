50117e4563e237b5fccef8293947492a76d44cc3
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-05-08 20:37:51 +0200

    Heart button in bottom right corner

--- SpiewnikWedrowca/SongView/SongView.swift
+++ SpiewnikWedrowca/SongView/SongView.swift
@@ -23,38 +23,46 @@ struct SongView: View {
     }
     
     var body: some View {
-        VStack {
-            List {
-                Text(vm.title)
-                .font(.system(size: 20, weight: .bold, design: .default))
-                ForEach(self.vm.songParts, id: \.self) { (song) in
-                    VStack (alignment: .leading, spacing: 16) {
-                        Text(song.title)
-                            .font(.system(size: 17, weight: .bold, design: .default))
-                        Text(song.text)
-                            .font(.system(size: 17, weight: .regular, design: .default))
-                            .lineSpacing(6)
+        ZStack {
+            VStack {
+                List {
+                    Text(vm.title)
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    ForEach(self.vm.songParts, id: \.self) { (song) in
+                        VStack (alignment: .leading, spacing: 16) {
+                            Text(song.title)
+                                .font(.system(size: 17, weight: .bold, design: .default))
+                            Text(song.text)
+                                .font(.system(size: 17, weight: .regular, design: .default))
+                                .lineSpacing(6)
+                        }
+                        
                     }
                     
                 }
+                .navigationBarTitle(Text(""), displayMode: .inline)
+            }
                 
+            .onAppear {
+                self.isLiked = self.db.likedSongs.contains(self.vm.song)
             }
-            .navigationBarTitle(Text(""), displayMode: .inline)
-            Button(action: {
-                self.isLiked.toggle()
-                self.shouldAnimate = true
-                print(self.isLiked)
-            }) {
-                LottieButton(isPressed: $isLiked, shouldAnimate: $shouldAnimate, from: 0.3, to: 1.0, filename: .heartAnimation)
-                    .frame(width: 150, height: 150, alignment: .center)
+            .onDisappear() {
+                self.isLiked ? self.db.add(self.vm.song) : self.db.remove(self.vm.song)
             }
-            
-        }
-        .onAppear {
-            self.isLiked = self.db.likedSongs.contains(self.vm.song)
-        }
-        .onDisappear() {
-            self.isLiked ? self.db.add(self.vm.song) : self.db.remove(self.vm.song)
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        self.isLiked.toggle()
+                        self.shouldAnimate = true
+                        print(self.isLiked)
+                    }) {
+                        LottieButton(isPressed: $isLiked, shouldAnimate: $shouldAnimate, from: 0.3, to: 1.0, filename: .heartAnimation)
+                            .frame(width: 150, height: 150, alignment: .center)
+                    }
+                }
+            }
         }
             
     }
