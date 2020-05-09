50b3d884ca8762fd3f774856a7137649036c6da3
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-05-09 20:41:58 +0200

    Confirmation popup when user taps trash in LikedSongsView

--- SpiewnikWedrowca/LikedSongs/LikedSongsView.swift
+++ SpiewnikWedrowca/LikedSongs/LikedSongsView.swift
@@ -11,32 +11,49 @@ import SwiftUI
 struct LikedSongsView: View {
     
     @ObservedObject var db: DatabaseManager
+    @State var showDeleteConfirmation: Bool = false
     
     var body: some View {
-        List {
-            ForEach(db.likedSongs, id: \.self) { song in
-                NavigationLink(destination: SongView(song, self.db)) {
-                    HStack {
-                        Text("\(song.number).")
-                        Text(song.title)
+        ZStack {
+            VStack {
+                List {
+                    ForEach(db.likedSongs, id: \.self) { song in
+                        NavigationLink(destination: SongView(song, self.db)) {
+                            HStack {
+                                Text("\(song.number).")
+                                Text(song.title)
+                            }
+                        }
                     }
+                    .onDelete(perform: db.removeSong)
                 }
+                .blur(radius: self.showDeleteConfirmation ? 10 : 0)
+                .onAppear(perform: {
+                    self.db.loadSongs()
+                })
+                    .navigationBarTitle(Text("Ulubione pieśni"))
+                    .blur(radius: self.showDeleteConfirmation ? 10 : 0)
+                    .navigationBarItems(trailing:
+                        Button(action: {
+                            if !self.db.likedSongs.isEmpty {
+                                self.showDeleteConfirmation.toggle()
+                            }
+                        }, label: {
+                            Icon(image: .trash, size: .medium, weight: .semibold, color: .main)
+                            .opacity(self.db.likedSongs.isEmpty ? 0.0 : 1.0)
+                        })
+                )
             }
-            .onDelete(perform: db.removeSong)
-        }
-        .onAppear(perform: {
-            self.db.loadSongs()
-        })
-        .navigationBarTitle(Text("Ulubione pieśni"))
-        .navigationBarItems(trailing:
-            Button(action: {
+            SingleActionPopup(showView: $showDeleteConfirmation, icon: .trash, message: "Czy na pewno chcesz usunąć wszystkie polubione pieśni?", buttonTitle: "Akceptuj") {
                 self.db.removeAll()
-            }, label: {
-                Icon(image: .trash, size: .medium, weight: .semibold, color: .main)
-            })
-        )
+                self.showDeleteConfirmation.toggle()
+            }
+            .scaleEffect(self.showDeleteConfirmation ? 1.0 : 0.5)
+            .opacity(self.showDeleteConfirmation ? 1.0 : 0.0)
+            .animation(.spring())
+        }
     }
-
+    
 }
 
 struct FavoriteSongsView_Previews: PreviewProvider {
