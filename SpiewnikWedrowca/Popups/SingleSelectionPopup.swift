b5db75bac1c05a62ee32c21ece2f690d325b5a89
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-05-09 19:09:04 +0200

    Make sure elements of popup don't animate

--- SpiewnikWedrowca/Popups/SingleSelectionPopup.swift
+++ SpiewnikWedrowca/Popups/SingleSelectionPopup.swift
@@ -25,15 +25,18 @@ struct SingleSelectionPopup: View {
                     .background(RoundedCorners(color: Colors.main.name, tl: 35, tr: 35, bl: 35, br: 35))
                     .shadow(color: Colors.main.name.opacity(0.7), radius: 6, x: 0, y: -4)
                     .offset(x: 0, y: -30)
+                    .animation(nil)
                 Text(message)
                     .font(.system(size: 16, weight: .regular, design: .default))
                     .padding(.top, -30)
+                    .animation(nil)
                 VStack {
                     Button(action: {
                         self.action?()
                     }) {
                         Text(buttonTitle)
                             .font(.system(size: 16, weight: .semibold, design: .default))
+                            .animation(nil)
                     }
                     .frame(width: UIScreen.width*2/3 - 32, height: 50, alignment: .center)
                     .foregroundColor(Colors.background.name)
@@ -43,6 +46,7 @@ struct SingleSelectionPopup: View {
                     }) {
                         Text("Anuluj")
                             .font(.system(size: 16, weight: .semibold, design: .default))
+                            .animation(nil)
                     }
                     .foregroundColor(Colors.main.name)
                     .frame(width: UIScreen.width*2/3 - 32, height: 50, alignment: .center)
